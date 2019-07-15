f10a65900cff59e80a1ade23a3ccff9ed2f4f8f6
Adriano Costa <iano76@gmail.com>
2019-07-15 16:37:51 +0200

    login form

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
-})
+import { AuthenticationService } from '../_services/authentication.service';
+
+@Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  // convenience getter for easy access to form fields
+  get formControls() {
+    return this.loginForm.controls;
   }
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentEmployeeValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService
+      .login(this.formControls.username.value, this.formControls.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          // TODO: show server error message to the user
+          // this.alertService.error(error);
+          console.log('error:', error);
+          this.loading = false;
+        }
+      );
+  }
 }
