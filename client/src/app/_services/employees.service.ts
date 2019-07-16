3cc166ebd57a50212a6e056cc949f690e6c5a2a3
Adriano Costa <iano76@gmail.com>
2019-07-16 12:26:48 +0200

    getAll()

--- client/src/app/_services/employees.service.ts
+++ client/src/app/_services/employees.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../_helpers/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    return this.http.get(`${config.apiUrl}/employees/`);
+  }
 }
