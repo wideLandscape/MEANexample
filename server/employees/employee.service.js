0dea77a9506bd46a43c3a6dabea9571d482f2d50
Adriano Costa <iano76@gmail.com>
2019-07-16 11:09:59 +0200

    use mongodb for authentication

--- server/employees/employee.service.js
+++ server/employees/employee.service.js
@@ -1,39 +1,35 @@
 const config = require('../config.json');
 const jwt = require('jsonwebtoken');
 
-// TODO: move employees to mongoDB
-const employees = [
-  {
-    id: 1,
-    username: 'test',
-    password: 'test',
-    firstName: 'Test',
-    lastName: 'employee',
-    isAdmin: true
-  }
-];
+// Require Employees model in our routes module
+const Employees = require('./employee.model');
 
 async function authenticate({ username, password }) {
-  const employee = employees.find(
-    u => u.username === username && u.password === password
-  );
-  if (employee) {
-    const token = jwt.sign({ sub: employee.id }, config.secret);
+  try {
     // don't return the password to the client
-    const { password, ...employeeWithoutPassword } = employee;
-    return {
-      ...employeeWithoutPassword,
-      token
-    };
+    const employee = await Employees.findOne(
+      { username, password },
+      { password: 0 }
+    );
+    if (employee) {
+      const token = jwt.sign({ sub: employee.id }, config.secret);
+      return {
+        ...employee.toObject(),
+        token
+      };
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
 
 async function getAll() {
-  return employees.map(u => {
+  try {
     // don't return the password to the client
-    const { password, ...employeeWithoutPassword } = u;
-    return employeeWithoutPassword;
-  });
+    return await Employees.find({}, { password: 0 });
+  } catch (err) {
+    console.log(err);
+  }
 }
 module.exports = {
   authenticate,
