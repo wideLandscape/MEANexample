b48b7604758fb76e2461911049d1d2c2193c42e6
Adriano Costa <iano76@gmail.com>
2019-07-16 12:35:06 +0200

    add error interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent, AdminComponent],
   imports: [
@@ -17,7 +18,8 @@ import { JwtInterceptor } from './_helpers/jwt.interceptor';
     ReactiveFormsModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
