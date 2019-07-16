1bbd6c0d53b21a0f6c13d01574230d9b9ff1536d
Adriano Costa <iano76@gmail.com>
2019-07-16 15:47:25 +0200

    implement employee form

--- client/src/app/admin/employee-form/employee-form.component.ts
+++ client/src/app/admin/employee-form/employee-form.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
+import { EmployeesService } from '../../_services/employees.service';
+// import { AlertService } from '../../_services/authentication.service';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.sass']
 })
 export class EmployeeFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  // convenience getter for easy access to form fields
+  get formControls() {
+    return this.registerForm.controls;
   }
 
+  constructor(
+    private formBuilder: FormBuilder,
+    //  private alertService: AlertService,
+    private employeesService: EmployeesService
+  ) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      isAdmin: []
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.employeesService
+      .add(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          //   this.alertService.success('Registration successful', true);
+          this.loading = false;
+          this.registerForm.reset();
+        },
+        error => {
+          //       this.alertService.error(error);
+          this.loading = false;
+        }
+      );
+  }
 }
