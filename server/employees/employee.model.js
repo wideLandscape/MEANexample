a8e62d113c419dde84a84150af6914c2925fbac1
Adriano Costa <iano76@gmail.com>
2019-07-16 11:08:56 +0200

    mongodb Employee schema

---
+++ server/employees/employee.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Employee
+const Employee = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    firstName: {
+      type: String
+    },
+    lastName: {
+      type: String
+    },
+    password: {
+      type: String
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    collection: 'employees'
+  }
+);
+
+module.exports = mongoose.model('Employee', Employee);
