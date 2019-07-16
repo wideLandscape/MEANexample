bf60d579449839e10cf6d0734b7b9040b2d2b43d
Adriano Costa <iano76@gmail.com>
2019-07-16 18:26:18 +0200

    refactoring

--- client/src/app/_services/alert.service.ts
+++ client/src/app/_services/alert.service.ts
@@ -5,30 +5,22 @@ import { Observable, Subject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class AlertService {
   private subject = new Subject<any>();
-  private keepAfterNavigationChange = false;
 
   constructor(router: Router) {
     // clear alert message on route change
     router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
-        if (this.keepAfterNavigationChange) {
-          // only keep for a single location change
-          this.keepAfterNavigationChange = false;
-        } else {
-          // clear alert
-          this.subject.next();
-        }
+        // clear alert
+        this.subject.next();
       }
     });
   }
 
-  success(message: string, keepAfterNavigationChange = false) {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
+  success(message: string) {
     this.subject.next({ type: 'success', text: message });
   }
 
-  error(message: string, keepAfterNavigationChange = false) {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
+  error(message: string) {
     this.subject.next({ type: 'error', text: message });
   }
 
