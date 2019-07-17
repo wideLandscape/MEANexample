15e46e4fe60bab6fba189d7bc01a40bc6f989586
Adriano Costa <iano76@gmail.com>
2019-07-17 14:20:52 +0200

    create model Review

---
+++ client/src/app/_models/review.ts
@@ -0,0 +1,9 @@
+import { Employee } from './Employee';
+
+export class Review {
+  // tslint:disable-next-line: variable-name
+  _id: string;
+  employee: Employee;
+  questions: [];
+  active: boolean;
+}
