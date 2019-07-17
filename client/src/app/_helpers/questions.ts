0f506f14a43d15b5510310add9057be80d82073a
Adriano Costa <iano76@gmail.com>
2019-07-17 14:25:43 +0200

    create questions helper

---
+++ client/src/app/_helpers/questions.ts
@@ -0,0 +1,31 @@
+export interface Question {
+  id: number;
+  text: string;
+}
+
+export const questions: Question[] = [
+  {
+    id: 1,
+    text: 'Work quality'
+  },
+  {
+    id: 2,
+    text: 'Technical knoledgwe'
+  },
+  {
+    id: 3,
+    text: 'Teamwork'
+  },
+  {
+    id: 4,
+    text: 'Problem solving'
+  },
+  {
+    id: 5,
+    text: 'Self-management'
+  },
+  {
+    id: 6,
+    text: 'Financial/Resource management'
+  }
+];
