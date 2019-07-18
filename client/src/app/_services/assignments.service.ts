d4eae8f14ba9a650ec26b09df683266672b71f59
Adriano Costa <iano76@gmail.com>
2019-07-18 13:00:52 +0200

    implement AssignmentService add() + delete()

--- client/src/app/_services/assignments.service.ts
+++ client/src/app/_services/assignments.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Review } from '../_models/Review';
-
+import { config } from '../_helpers/config';
+import { HttpClient } from '@angular/common/http';
+import { Assignment } from '../_models/assignment';
 @Injectable({
   providedIn: 'root'
 })
 export class AssignmentsService {
   public current: Review;
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  add(assignment: Assignment) {
+    return this.http.post(`${config.apiUrl}/assignments/add`, assignment);
+  }
+
+  delete(id: string) {
+    return this.http.get(`${config.apiUrl}/assignments/delete/${id}`);
+  }
 }
