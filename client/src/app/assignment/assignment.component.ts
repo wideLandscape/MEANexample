1259df9b2667f916db30e8bb68a63ada546c62da
Adriano Costa <iano76@gmail.com>
2019-07-18 12:02:19 +0200

    add form for reviewer assignment

--- client/src/app/assignment/assignment.component.ts
+++ client/src/app/assignment/assignment.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AssignmentsService } from '../_services/assignments.service';
 import { Review } from '../_models/Review';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Employee } from '../_models/Employee';
+import { first } from 'rxjs/operators';
+import { EmployeesService } from '../_services/employees.service';
 
 @Component({
   selector: 'app-assignment',
@@ -10,13 +14,49 @@ import { Router } from '@angular/router';
 })
 export class AssignmentComponent implements OnInit {
   review: Review = this.assignmentsService.current;
-
-  constructor(private assignmentsService: AssignmentsService, router: Router) {
+  assignForm: FormGroup;
+  loading = false;
+  submitted = false;
+  employees: Employee[];
+  constructor(
+    private assignmentsService: AssignmentsService,
+    private formBuilder: FormBuilder,
+    private employeesService: EmployeesService,
+    router: Router
+  ) {
     // redirect to home if already logged in
     if (!this.assignmentsService.current) {
       router.navigate(['/review']);
     }
   }
+  // convenience getter for easy access to form fields
+  get formControls() {
+    return this.assignForm.controls;
+  }
+  ngOnInit() {
+    this.assignForm = this.formBuilder.group({
+      employee: [null, Validators.required]
+    });
+    this.getEmployees();
+  }
 
-  ngOnInit() {}
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.assignForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+  }
+
+  private getEmployees() {
+    this.employeesService
+      .getAll()
+      .pipe(first())
+      .subscribe((data: Employee[]) => {
+        this.employees = data;
+      });
+  }
 }
