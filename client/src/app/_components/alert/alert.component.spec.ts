195daa1f638f8a8ca536ea7a7c11d30aa707e85c
Adriano Costa <iano76@gmail.com>
2019-07-23 16:43:49 +0200

    refactoring

--- client/src/app/_components/alert/alert.component.spec.ts
+++ client/src/app/_components/alert/alert.component.spec.ts
@@ -1,12 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
 import { AlertService } from 'src/app/_services/alert.service';
 import { RouterTestingModule } from '@angular/router/testing';
 describe('AlertComponent', () => {
-  let component: AlertComponent;
-  let fixture: ComponentFixture<AlertComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -14,14 +11,19 @@ describe('AlertComponent', () => {
     }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AlertComponent);
-    component = fixture.componentInstance;
-    fixture.debugElement.injector.get(AlertService);
-    fixture.detectChanges();
-  });
+  function setup() {
+    const fixture: ComponentFixture<AlertComponent> = TestBed.createComponent(
+      AlertComponent
+    );
+    const component: AlertComponent = fixture.componentInstance;
+    const alertService: AlertService = fixture.debugElement.injector.get(
+      AlertService
+    );
+    return { fixture, component, alertService };
+  }
 
   it('should create', () => {
+    const { component } = setup();
     expect(component).toBeTruthy();
   });
 });
