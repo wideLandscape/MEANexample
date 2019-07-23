2b2ca4cba4d49920d4b0e610fc335b0f3a5087ba
Adriano Costa <iano76@gmail.com>
2019-07-23 17:30:32 +0200

    test AlertComponent

--- client/src/app/_components/alert/alert.component.spec.ts
+++ client/src/app/_components/alert/alert.component.spec.ts
@@ -1,8 +1,16 @@
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+  ComponentFixture,
+  tick,
+  fakeAsync
+} from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
-import { AlertService } from 'src/app/_services/alert.service';
+import { AlertService, Message } from 'src/app/_services/alert.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, Observer } from 'rxjs';
+
 describe('AlertComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,11 +27,39 @@ describe('AlertComponent', () => {
     const alertService: AlertService = fixture.debugElement.injector.get(
       AlertService
     );
+
     return { fixture, component, alertService };
   }
 
   it('should create', () => {
     const { component } = setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should display a message', fakeAsync(() => {
+    const { fixture, alertService } = setup();
+    const mockMessage: Message = { type: 'success', text: 'Ciao' };
+    spyOn(alertService, 'getMessage').and.returnValue(
+      Observable.create((observer: Observer<Message>) => {
+        observer.next(mockMessage);
+        return observer;
+      })
+    );
+
+    tick();
+
+    fixture.detectChanges();
+    const alertComponentElement = fixture.debugElement.nativeElement;
+    const alertMessage = alertComponentElement.querySelector('div');
+    expect(alertMessage.textContent.trim()).toContain('Ciao');
+  }));
+
+  it('close() should hide', () => {
+    const { fixture, component } = setup();
+    component.close();
+    fixture.detectChanges();
+    const alertComponentElement = fixture.debugElement.nativeElement;
+    const alertMessage = alertComponentElement.querySelector('div');
+    expect(alertMessage).toBeFalsy();
   });
 });
