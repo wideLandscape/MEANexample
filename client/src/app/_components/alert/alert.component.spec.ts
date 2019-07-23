5a47ef083b86d9ef43b4593aa0e48a4975f76fe3
Adriano Costa <iano76@gmail.com>
2019-07-23 16:38:12 +0200

    test AlertComponent - it should create

--- client/src/app/_components/alert/alert.component.spec.ts
+++ client/src/app/_components/alert/alert.component.spec.ts
@@ -1,21 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
-
+import { AlertService } from 'src/app/_services/alert.service';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [AlertComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
+    fixture.debugElement.injector.get(AlertService);
     fixture.detectChanges();
   });
 
