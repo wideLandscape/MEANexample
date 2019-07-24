f23f21e748794b263d18d1e22e1b9e2efd8acd45
Adriano Costa <iano76@gmail.com>
2019-07-24 16:30:53 +0200

    test AlertService

--- client/src/app/_services/alert.service.spec.ts
+++ client/src/app/_services/alert.service.spec.ts
@@ -1,12 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AlertService } from './alert.service';
+import { AlertService, Message } from './alert.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AlertService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({ imports: [RouterTestingModule] })
+  );
 
   it('should be created', () => {
     const service: AlertService = TestBed.get(AlertService);
     expect(service).toBeTruthy();
+  });
+  it('should send success messages', () => {
+    const service: AlertService = TestBed.get(AlertService);
+    const messages$ = service.getMessage();
+    expect(messages$).toBeTruthy();
+    messages$.subscribe((message: Message) => {
+      expect(message.text === 'success!!').toBeTruthy();
+      expect(message.type === 'success').toBeTruthy();
+    });
+
+    service.success('success!!');
+  });
+  it('should send error messages', () => {
+    const service: AlertService = TestBed.get(AlertService);
+    const messages$ = service.getMessage();
+    expect(messages$).toBeTruthy();
+    messages$.subscribe((message: Message) => {
+      expect(message.text === 'error!!').toBeTruthy();
+      expect(message.type === 'error').toBeTruthy();
+    });
+
+    service.error('error!!');
+  });
+
+  it('close() should send undefined messages', () => {
+    const service: AlertService = TestBed.get(AlertService);
+    const messages$ = service.getMessage();
+    expect(messages$).toBeTruthy();
+    messages$.subscribe((message: Message) => {
+      expect(message === undefined).toBeTruthy();
+    });
+    service.close();
   });
 });
