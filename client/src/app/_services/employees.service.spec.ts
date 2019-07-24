4f334a7972107e66fbdf994a8d41df6f604e157d
Adriano Costa <iano76@gmail.com>
2019-07-24 16:48:20 +0200

    test services with httpModule

--- client/src/app/_services/employees.service.spec.ts
+++ client/src/app/_services/employees.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeesService } from './employees.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('EmployeesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  function setup() {
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+  }
 
   it('should be created', () => {
+    setup();
     const service: EmployeesService = TestBed.get(EmployeesService);
     expect(service).toBeTruthy();
   });
--- client/src/app/_services/reviews.service.spec.ts
+++ client/src/app/_services/reviews.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReviewsService } from './reviews.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ReviewsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  function setup() {
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+  }
   it('should be created', () => {
+    setup();
     const service: ReviewsService = TestBed.get(ReviewsService);
     expect(service).toBeTruthy();
   });
