02248a7ba295e2bc0a3395d18493795fdb58774e
Adriano Costa <iano76@gmail.com>
2019-07-24 16:38:37 +0200

    test AssignmentsService

--- client/src/app/_services/assignments.service.spec.ts
+++ client/src/app/_services/assignments.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AssignmentsService } from './assignments.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AssignmentsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  function setup() {
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+  }
 
   it('should be created', () => {
+    setup();
     const service: AssignmentsService = TestBed.get(AssignmentsService);
     expect(service).toBeTruthy();
   });
