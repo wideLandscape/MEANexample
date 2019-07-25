bb0010c62c2f63c61c06fdda90908cf952e2dd6b
Adriano Costa <iano76@gmail.com>
2019-07-25 17:26:14 +0200

    test EmployeeFormComponent

--- client/src/app/employee/employee-form/employee-form.component.spec.ts
+++ client/src/app/employee/employee-form/employee-form.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { EmployeeFormComponent } from './employee-form.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeesService } from 'src/app/_services/employees.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Employee } from 'src/app/_models/employee';
+const baseEmployee: Employee = {
+  _id: '1',
+  username: 'username',
+  firstName: 'Remy',
+  lastName: 'Penchenat',
+  token: 'token',
+  isAdmin: false
+};
 describe('EmployeeFormComponent', () => {
-  let component: EmployeeFormComponent;
-  let fixture: ComponentFixture<EmployeeFormComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeFormComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      declarations: [EmployeeFormComponent]
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EmployeeFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  function setup() {
+    const fixture = TestBed.createComponent(EmployeeFormComponent);
+    const component = fixture.debugElement.componentInstance;
+    const employeesService: EmployeesService = fixture.debugElement.injector.get(
+      EmployeesService
+    );
+
+    // TODO: how to test Angular Input('employee') setter ????
+    //  component.editableEmployee = baseEmployee;
+    //  fixture.detectChanges();
+    return { fixture, component, employeesService };
+  }
 
   it('should create', () => {
+    const { component } = setup();
     expect(component).toBeTruthy();
   });
 });
