4298e830375feee309791ff09b81838bbba114fb
Adriano Costa <iano76@gmail.com>
2019-07-25 18:25:36 +0200

    refactor

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { AuthenticationService } from './_services/authentication.service';
 import { AlertComponent } from './_components/alert/alert.component';
 import { HttpClientModule } from '@angular/common/http';
 
