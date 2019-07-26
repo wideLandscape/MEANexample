e0359b3cdf76dd52802d3124b3b2b279dbe9e7bd
Adriano Costa <iano76@gmail.com>
2019-07-26 15:12:57 +0200

    test ReviewComponent

--- client/src/app/review/review.component.spec.ts
+++ client/src/app/review/review.component.spec.ts
@@ -1,25 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReviewComponent } from './review.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ReviewFormComponent } from './review-form/review-form.component';
 
 describe('ReviewComponent', () => {
-  let component: ReviewComponent;
-  let fixture: ComponentFixture<ReviewComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReviewComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [ReviewComponent, ReviewFormComponent]
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ReviewComponent);
-    component = fixture.componentInstance;
+  function setup() {
+    const fixture = TestBed.createComponent(ReviewComponent);
+    const component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
-  });
-
+    return { fixture, component };
+  }
   it('should create', () => {
+    const { component } = setup();
     expect(component).toBeTruthy();
   });
 });
