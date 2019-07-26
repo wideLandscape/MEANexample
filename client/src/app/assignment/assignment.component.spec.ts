26801d3ecee2ba37108a91c41402d3518d203492
Adriano Costa <iano76@gmail.com>
2019-07-26 15:59:30 +0200

    test AssignmentComponent add spyOn

--- client/src/app/assignment/assignment.component.spec.ts
+++ client/src/app/assignment/assignment.component.spec.ts
@@ -7,6 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { Employee } from '../_models/employee';
 import { Review } from '../_models/Review';
 import { AssignmentsService } from '../_services/assignments.service';
+import { Router } from '@angular/router';
+import { EmployeesService } from '../_services/employees.service';
+import { Observable, Observer, of } from 'rxjs';
+
+class MockRouter {
+  events = of(null);
+  routerState = { root: undefined };
+  navigate(path) {}
+}
 
 const baseEmployee: Employee = {
   _id: '1',
@@ -35,6 +44,7 @@ describe('AssignmentComponent', () => {
         RouterTestingModule,
         HttpClientModule
       ],
+      providers: [{ provide: Router, useClass: MockRouter }],
       declarations: [AssignmentComponent]
     }).compileComponents();
   }));
@@ -44,12 +54,52 @@ describe('AssignmentComponent', () => {
       AssignmentComponent
     > = TestBed.createComponent(AssignmentComponent);
     const component: AssignmentComponent = fixture.componentInstance;
-    const assignmentService: AssignmentsService = fixture.debugElement.injector.get(
+    const assignmentsService: AssignmentsService = fixture.debugElement.injector.get(
       AssignmentsService
     );
-    assignmentService.current = review;
+    assignmentsService.current = review;
+    component.review = assignmentsService.current;
+    const mockReviewers: any[] = [
+      {
+        _id: '1',
+        review_id: '1',
+        employee_id: baseEmployee,
+        results: [],
+        done: false
+      },
+      {
+        _id: '2',
+        employee_id: baseEmployee,
+        results: [],
+        done: false
+      }
+    ];
+    spyOn(assignmentsService, 'reviewers').and.returnValue(
+      Observable.create((observer: Observer<any[]>) => {
+        observer.next(mockReviewers);
+        return observer;
+      })
+    );
+    const mockEmployees: any[] = [
+      baseEmployee,
+      baseEmployee,
+      baseEmployee,
+      baseEmployee
+    ];
 
-    component.review = assignmentService.current; // TODO: this is weird
+    const employeesService: EmployeesService = fixture.debugElement.injector.get(
+      EmployeesService
+    );
+
+    spyOn(employeesService, 'getAll').and.returnValue(
+      Observable.create((observer: Observer<any[]>) => {
+        observer.next(mockEmployees);
+        return observer;
+      })
+    );
+    const router = TestBed.get(Router);
+    spyOn(router, 'navigate').and.returnValue(null);
+
     fixture.detectChanges();
     return { fixture, component };
   }
