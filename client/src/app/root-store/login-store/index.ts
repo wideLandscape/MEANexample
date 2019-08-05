7f6cf52727749f7624a476d3b7bd66a63e2478b6
Adriano Costa <iano76@gmail.com>
2019-08-05 10:39:47 +0200

    barrel export

---
+++ client/src/app/root-store/login-store/index.ts
@@ -0,0 +1,7 @@
+import * as LoginStoreActions from './actions';
+import * as LoginStoreSelectors from './selectors';
+import * as LoginStoreState from './state';
+
+export { LoginStoreModule } from './login-store.module';
+
+export { LoginStoreActions, LoginStoreSelectors, LoginStoreState };
