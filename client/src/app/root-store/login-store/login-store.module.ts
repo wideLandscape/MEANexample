56d9755d1ea9f3fb84f4678320411e726efb611e
Adriano Costa <iano76@gmail.com>
2019-08-05 10:39:19 +0200

    add store and effects to module

--- client/src/app/root-store/login-store/login-store.module.ts
+++ client/src/app/root-store/login-store/login-store.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { LoginStoreEffects } from './effects';
+import { featureReducer } from './reducer';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    StoreModule.forFeature('login', featureReducer),
+    EffectsModule.forFeature([LoginStoreEffects])
+  ],
+  providers: [LoginStoreEffects]
 })
-export class LoginStoreModule { }
+export class LoginStoreModule {}
