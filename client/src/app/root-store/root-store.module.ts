e121003443fe80a1887e094589fc73ad542d87ac
Adriano Costa <iano76@gmail.com>
2019-08-06 18:03:18 +0200

    add login Module to root store module

--- client/src/app/root-store/root-store.module.ts
+++ client/src/app/root-store/root-store.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginStoreModule } from './login-store/login-store.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, LoginStoreModule]
+  imports: [
+    CommonModule,
+    LoginStoreModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([])
+  ]
 })
 export class RootStoreModule {}
