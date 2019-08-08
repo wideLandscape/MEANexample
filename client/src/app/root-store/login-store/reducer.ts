cbf31ea37c93daf15f3dc9e5483a8561765ffe37
Adriano Costa <iano76@gmail.com>
2019-08-08 11:49:43 +0200

    refactoring

--- client/src/app/root-store/login-store/reducer.ts
+++ client/src/app/root-store/login-store/reducer.ts
@@ -10,39 +10,41 @@ import { initialState, State } from './state';
 interface IReducer {
   [key: string]: (state: State, action: Actions) => State;
 }
-const reducers: IReducer = {};
 
-reducers[ActionTypes.LOGIN_REQUEST] = (
-  state: State,
-  action: LoginRequestAction
-) => ({
-  ...state,
-  error: null,
-  isLoading: true
-});
-
-reducers[ActionTypes.LOGIN_SUCCESS] = (
-  state: State,
-  action: LoginSuccessAction
-) => ({
-  ...state,
-  user: action.payload.user,
-  error: null,
-  isLoading: false
-});
-
-reducers[ActionTypes.LOGIN_FAILURE] = (
-  state: State,
-  action: LoginFailureAction
-) => ({
-  ...state,
-  error: action.payload.error,
-  isLoading: false
-});
+const getReducers = () => {
+  const reducers: IReducer = {};
+  reducers[ActionTypes.LOGIN_REQUEST] = (
+    state: State,
+    action: LoginRequestAction
+  ) => ({
+    ...state,
+    error: null,
+    isLoading: true
+  });
+  reducers[ActionTypes.LOGIN_SUCCESS] = (
+    state: State,
+    action: LoginSuccessAction
+  ) => ({
+    ...state,
+    user: action.payload.user,
+    error: null,
+    isLoading: false
+  });
+  reducers[ActionTypes.LOGIN_FAILURE] = (
+    state: State,
+    action: LoginFailureAction
+  ) => ({
+    ...state,
+    error: action.payload.error,
+    isLoading: false
+  });
+  return reducers;
+};
 
 export function featureReducer(
   state: State = initialState,
   action: Actions
 ): State {
+  const reducers: IReducer = getReducers();
   return reducers[action.type] ? reducers[action.type](state, action) : state;
 }
