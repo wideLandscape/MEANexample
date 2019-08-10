2ce26b4033675c5fc9f25522b5d3030174f4386b
Adriano Costa <iano76@gmail.com>
2019-08-10 12:29:33 +0200

    use ngrx store for jwt interceptor

--- client/src/app/_helpers/jwt.interceptor.ts
+++ client/src/app/_helpers/jwt.interceptor.ts
@@ -6,19 +6,28 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-import { AuthenticationService } from '../_services/authentication.service';
+import { RootStoreState, LoginStoreSelectors } from '../root-store';
+import { Store } from '@ngrx/store';
+import { first, flatMap } from 'rxjs/operators';
+import { Employee } from '../_models/employee';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private store$: Store<RootStoreState.State>) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
-    const token = this.getToken();
+    return this.store$.select(LoginStoreSelectors.selectLoginUser).pipe(
+      first(),
+      flatMap((user: Employee) => next.handle(this.getRequest(user, request)))
+    );
+  }
+
+  private getRequest(user: Employee, request: HttpRequest<any>) {
+    const token = user ? user.token : null;
     if (token) {
       request = request.clone({
         setHeaders: {
@@ -26,13 +35,6 @@ export class JwtInterceptor implements HttpInterceptor {
         }
       });
     }
-    return next.handle(request);
-  }
-
-  getToken() {
-    const currentEmployee = this.authenticationService.currentEmployeeValue;
-    return currentEmployee && currentEmployee.token
-      ? currentEmployee.token
-      : null;
+    return request;
   }
 }
