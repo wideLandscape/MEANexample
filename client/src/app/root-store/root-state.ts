4164fe0c529fb3c7ac8ecbd0c995eaed8006f668
Adriano Costa <iano76@gmail.com>
2019-08-11 16:32:14 +0200

    add ReviewStoreModule to RootStoreModule

--- client/src/app/root-store/root-state.ts
+++ client/src/app/root-store/root-state.ts
@@ -1,5 +1,6 @@
 import { LoginStoreState } from './login-store';
-
+import { State as ReviewState } from './review-store/review/review.reducer';
 export interface State {
   login: LoginStoreState.State;
+  review: ReviewState;
 }
--- client/src/app/root-store/root-store.module.ts
+++ client/src/app/root-store/root-store.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginStoreModule } from './login-store/login-store.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { ReviewStoreModule } from './review-store/review-store.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     LoginStoreModule,
+    ReviewStoreModule,
     StoreModule.forRoot(
       {},
       {
