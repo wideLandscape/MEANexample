545918940ec8c253b31815710fc01d48ae46248b
Adriano Costa <iano76@gmail.com>
2019-08-13 12:16:01 +0200

    add RootStoreActions to barrel export

--- client/src/app/root-store/index.ts
+++ client/src/app/root-store/index.ts
@@ -2,6 +2,13 @@ import { RootStoreModule } from './root-store.module';
 import * as RootStoreSelectors from './selectors';
 import * as RootStoreState from './root-state';
 
+import * as RootStoreActions from './root-store.actions';
+
 export * from './review-store/review';
 export * from './auth-store/auth';
-export { RootStoreState, RootStoreSelectors, RootStoreModule };
+export {
+  RootStoreState,
+  RootStoreSelectors,
+  RootStoreModule,
+  RootStoreActions
+};
