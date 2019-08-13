4a7005c560fb3846ff3a1a2a1b202d2f2bb0e8c3
Adriano Costa <iano76@gmail.com>
2019-08-13 12:15:40 +0200

    add RootStoreEffects to EffectsModule.forRoot([])

--- client/src/app/root-store/root-store.module.ts
+++ client/src/app/root-store/root-store.module.ts
@@ -4,6 +4,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { ReviewStoreModule } from './review-store/review-store.module';
 import { AuthStoreModule } from './auth-store/auth-store.module';
+import { RootStoreEffects } from './root-store.effects';
 
 @NgModule({
   declarations: [],
@@ -20,7 +21,7 @@ import { AuthStoreModule } from './auth-store/auth-store.module';
         }
       }
     ),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([RootStoreEffects])
   ]
 })
 export class RootStoreModule {}
