98370e773bac2b24c63a8cca5c70b8c4bc9e689e
Adriano Costa <iano76@gmail.com>
2019-08-15 16:53:18 +0200

    refactoring

--- client/src/app/root-store/review-store/review/review.selectors.ts
+++ client/src/app/root-store/review-store/review/review.selectors.ts
@@ -7,28 +7,24 @@ import {
 import { reviewsFeatureKey } from './review.reducer';
 import { Review } from './review.model';
 
-// SELECTORS AVAILABLE OUT OF THE BOX
-/*
-export const {
-  selectIds,
-  selectEntities,
-  selectAll,
-  selectTotal
-} = adapter.getSelectors();
-*/
-
-export const getError = (state: State): any => state.error;
-
-export const getIsLoading = (state: State): boolean => state.isLoading;
-
 export const selectReviewState: MemoizedSelector<
   object,
   State
 > = createFeatureSelector<State>(reviewsFeatureKey);
 
-export const selectReviewItems: (
-  state: object
-) => Review[] = adapter.getSelectors(selectReviewState).selectAll;
+// SELECTORS AVAILABLE OUT OF THE BOX
+const {
+  // selectIds,
+  // selectEntities,
+  selectAll
+  // selectTotal
+} = adapter.getSelectors(selectReviewState);
+
+export const getError = (state: State): any => state.error;
+
+export const getIsLoading = (state: State): boolean => state.isLoading;
+
+export const selectReviewItems: (state: object) => Review[] = selectAll;
 
 export const selectReviewError: MemoizedSelector<object, any> = createSelector(
   selectReviewState,
