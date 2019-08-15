186a8dba3a66a531eb3372f4cec482b53357687c
Adriano Costa <iano76@gmail.com>
2019-08-15 17:41:09 +0200

    add selectors

--- client/src/app/root-store/employee-store/employee/employee.selectors.ts
+++ client/src/app/root-store/employee-store/employee/employee.selectors.ts
@@ -1,8 +1,43 @@
-import { adapter } from './employee.state';
+import { adapter, State } from './employee.state';
+import {
+  MemoizedSelector,
+  createFeatureSelector,
+  createSelector
+} from '@ngrx/store';
+import { employeesFeatureKey } from './employee.reducer';
+import { Employee } from './employee.model';
 
-export const {
-  selectIds,
-  selectEntities,
-  selectAll,
-  selectTotal
-} = adapter.getSelectors();
+export const selectEmployeeState: MemoizedSelector<
+  object,
+  State
+> = createFeatureSelector<State>(employeesFeatureKey);
+
+// SELECTORS AVAILABLE OUT OF THE BOX
+const {
+  // selectIds,
+  // selectEntities,
+  selectAll
+  // selectTotal
+} = adapter.getSelectors(selectEmployeeState);
+
+export const getError = (state: State): any => state.error;
+
+export const getIsLoading = (state: State): boolean => state.isLoading;
+
+export const selectEmployeeItems: (state: object) => Employee[] = selectAll;
+
+export const selectEmployeeError: MemoizedSelector<
+  object,
+  any
+> = createSelector(
+  selectEmployeeState,
+  getError
+);
+
+export const selectEmployeeIsLoading: MemoizedSelector<
+  object,
+  boolean
+> = createSelector(
+  selectEmployeeState,
+  getIsLoading
+);
